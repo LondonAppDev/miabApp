8e954f188fe8e1c89a644da69f958a0e5db18306
Mark Winterbottom <mark@winterbottom.me>
2016-05-04 16:13:17 -0500

    Added comments to the services file.

--- www/js/services.js
+++ www/js/services.js
@@ -5,14 +5,21 @@ var app = angular.module('miabApp.services', []);
 app.service('WebService', function($http) {
 
     // This is the base URL for our REST API. Since we will be using the
-    // ionic proxy
+    // ionic proxy, we can exclude the FQDN and port number.
     var WS_BASE_URL = '/api';
 
+    // All 'puclic' functions need to be inside a return statement.
     return {
+
+        // This is our function for trading a message.
         tradeMessage: function(message, onSuccess, onError) {
+            // Create our payload variable called 'data'.
             var data = {'message': message};
+            // Use $http to post to our /message endpoint.
             $http.post(WS_BASE_URL + '/message/', data).then(
+                // Call the onSuccess function if it's successful.
                 onSuccess,
+                // Call the onError function if it fails.
                 onError
             );
         }
